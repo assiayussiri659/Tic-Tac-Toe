06d5210791d4d85eeba2aff984e89cbacd73e681
assiayussiri659 <ayushdubey2053@gmail.com>
2024-12-20 12:12:55 +0530

    m

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -32,22 +32,30 @@ export const TicTacToe = () => {
         {
             won(data);  
         }
-        else if(data[3]===data[4] && data[4]===data[5] && data[5]!=="");
+        else if(data[3]===data[4] && data[4]===data[5] && data[5]!=="")
         {
             won(data);
         }
-        else if(data[6]===data[7] && data[7]===data[8] && data[8]!=="");
+        else if(data[6]===data[7] && data[7]===data[8] && data[8]!=="")
             {
                 won(data);
             }
-        else if(data[0]===data[3] && data[3]===data[6] && data[6]!=="");
+        else if(data[0]===data[3] && data[3]===data[6] && data[6]!=="")
             {
                 won(data);
             }
-        else if(data[1]===data[4] && data[4]===data[7] && data[7]!=="");
+        else if(data[1]===data[4] && data[4]===data[7] && data[7]!=="")
             {
                 won(data);
             }
+            else if(data[2]===data[5] && data[5]===data[8] && data[8]!=="")
+                {
+                    won(data);
+                }
+                else if(data[0]===data[1] && data[1]===data[2] && data[2]!=="")
+                    {
+                        won(data);
+                    }
         }
         
 
